75ab29080bbde125fdac3da14986ea1ad13b94d0
rohini21m <67290441+rohini21m@users.noreply.github.com>
2026-09-17 16:32:37 -0500

    Update KPI_logic.sql

--- KPI_logic.sql
+++ KPI_logic.sql
@@ -1,6 +1,4 @@
 KPI_1 : baseline_per_category Logic : 
-
-create view account_spend_per_category_view as (
 SELECT 
     merchant_code, 
     merchant_code_description,
@@ -26,17 +24,13 @@ WHERE trnx_date >= '2025-01-01'
   AND f.merchant_code IN ('9135', '9144', '9147', '9149') 
 GROUP BY merchant_code, merchant_code_description, product_name
 
-) 
-select * from account_spend_per_category_view
-
-
+--------------------------------------------
 KPI2 : Transaction_Volume logic :
 ---transaction_volume : catgeory_trnx_count in 2025
-select f.merchant_code, f.merchant_code_description,
-p.product_name,
-count(trnx_id)filter(where f.merchant_code_description LIKE '%Groceries%') as total_captured_travel_trnxs,
+select p.product_name,Concat('Q',to_char(trnx_date,'Q')) as Quarters,
+count(trnx_id)filter(where f.merchant_code_description LIKE '%Groceries%') as total_captured_grocery_trnxs,
 count(trnx_id)filter(where f.merchant_code_description LIKE '%Streaming%') as total_captured_streaming_trnxs,
-count(trnx_id)filter(where f.merchant_code_description LIKE '%Restaurant%') as total_captured_streaming_trnxs
+count(trnx_id)filter(where f.merchant_code_description LIKE '%Restaurant%') as total_captured_Restaurant_trnxs
 from RFM_ANALYSIS.fact_transactions f 
 inner join RFM_ANALYSIS.dim_products p 
 on f.product_code=p.product_code 
@@ -44,7 +38,10 @@ where f.trnx_date>='01-01-2025'
 and f.trnx_date<='12-31-2025'
 and f.product_code in ('101','102') 
 and f.merchant_code in ('9135', '9144', '9147', '9149') 
-group by f.merchant_code, f.merchant_code_description,p.product_name  
+group by p.product_name ,to_char(trnx_date,'Q') 
+
+-----------------------------------
+KPI3 : 
 
 
 
